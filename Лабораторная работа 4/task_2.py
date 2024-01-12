fef50c51765ed2b12f073cff07eaeaed650e422a
ViktorUshkov <63870567+ViktorUshkov@users.noreply.github.com>
2024-01-12 15:00:06 +0300

    Лабораторная работа 4

---
+++ Лабораторная работа 4/task_2.py
@@ -0,0 +1,24 @@
+import json
+import csv
+
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    with open(INPUT_FILENAME, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        rows = [row for row in reader]
+
+    with open(OUTPUT_FILENAME, 'w') as json_file:
+        json.dump(rows, json_file, indent=4)
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
